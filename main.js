d539eee67bf194574319cd3d39d650991c23a191
Magomet Kochkarov <mkochkarov.fortech@gmail.com>
2022-04-13 10:20:59 +0300

    test git commit -a

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ console.log(sayHi)
 console.log(sayHi())
 console.log(test)
 console.log(test(41))
+
+var setName = function(name){
+    return name;
+}
+
+console.log(setName("Magomet"))
+
+try {
+    const temp = 4
+    temp = 1
+} catch (error) {
+    console.log(error)
+}
+
+console.log("done")
